8aadbc40899889be7e9abfa97c6e18861eb34150
Karl Bode <kpbode@gmail.com>
2012-10-11 08:52:23 +0200

    Initial Commit

---
+++ GiftIdeas/KPBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  KPBAppDelegate.h
+//  GiftIdeas
+//
+//  Created by Karl Bode on 11.10.12.
+//  Copyright (c) 2012 Karl Bode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ GiftIdeas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GiftIdeas
+//
+//  Created by Karl Bode on 11.10.12.
+//  Copyright (c) 2012 Karl Bode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KPBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KPBAppDelegate class]));
+    }
+}
